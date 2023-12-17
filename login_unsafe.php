b3ad09c6faf1eb115606ddc9b2c11a451c539b0a
GamalSilvio <gamalgullifa@gmail.com>
2023-12-17 22:33:11 +0000

    App vulnerable to SQL Injections and other attacks

---
+++ login_unsafe.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="login.php" method="post">
+        <input type="text" name="username" placeholder="Username" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <input type="submit" value="Log in">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $db = new SQLite3('users.db');
+        $result = $db->query("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
+
+        $row = $result->fetchArray(SQLITE3_ASSOC);
+        if ($row) {
+            echo "Welcome, you have logged in successfully " . $row['username'] . "!";
+        } else {
+            echo "User or password is incorrect.";
+        }
+    }
+    ?>
+</body>
+</html>
---
+++ signup_unsafe.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign up</title>
+</head>
+<body>
+    <h1>Sign up</h1>
+    <form action="signup.php" method="post">
+        <input type="text" name="username" placeholder="User" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <input type="submit" value="Sign up">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $db = new SQLite3('users.db');
+
+        // Vulnerable code: SQL injection
+        $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+        $result = $db->query($query);
+
+        if ($result) {
+            echo "User has signed up successfully.";
+        } else {
+            echo "Something went wrong with you sign up.";
+        }
+    }
+    ?>
+</body>
+</html>
