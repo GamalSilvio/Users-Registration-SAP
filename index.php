4640e106a943c3f7414e542473edb419a334ca3a
GamalSilvio <131446871+GamalSilvio@users.noreply.github.com>
2023-12-17 22:36:09 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Home</title>
-</head>
-<body>
-    <h1>Welcome</h1>
-    <a href="register.php">Sign up</a>
-    <a href="login.php">Login</a>
-</body>
-</html>
+
